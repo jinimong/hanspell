f209aeb9a9f7d56d647edd033e486b7013c66ac0
jwkim <phoongdeng@gmail.com>
2020-05-13 13:55:41 +0900

    spellcheck 기능 붙이기

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Spell-Checker' });
+});
+
+router.post('/', function(req, res, next) {
+  const hanspell = require('hanspell');
+  let data = [];
+  const check = function(json) { data = data.concat(json); }
+  const end = function () { res.json(data); };
+  const error = function (err) { console.error("// error: " + err); };
+
+  const sentence = req.body.sentence;
+  hanspell.spellCheckByDAUM(sentence, 6000, check, end, error);
 });
 
 module.exports = router;
